2ce156c6d4c73fa5a9cb8cb73094287f93f9ab71
BIGWJZ <1059537323@qq.com>
2022-09-22 19:45:27 +0800

    protobuf init.sh

--- proto_init.sh
+++ proto_init.sh
@@ -2,12 +2,14 @@
 
 set -ex
 
-tar -xzvf ./etc/protobuf-cpp-3.0.0.tar.gz
+ROOT_DIR = $(pwd)
+tar -xzvf ./etc/protobuf-cpp-3.0.0.tar.gz -C ./etc
 cd ./etc/protobuf-3.0.0
 cp -r ./src/google /usr/include
 ./configure --prefix=/usr/local/protobuf
 make
 make install
+cd $ROOT_DIR
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/protobuf/lib
 export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/protobuf/lib
 export PATH=$PATH:/usr/local/protobuf/bin
